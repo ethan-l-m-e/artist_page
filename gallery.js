aecb8dc20be89cf9ee65b0841dc4a3d7d7246fa4
ethan-l-m-e <ethan.l.m.e@icloud.com>
2021-12-18 00:03:51 +0800

    Added slideshow gallery

--- gallery.js
+++ gallery.js
@@ -27,11 +27,11 @@ function showSlides(n) {
         slideIndex = 1;
     }
     for (var i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none"
+        slides[i].style.display = 'none'
     }
     for (var i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(' active', '')
     }
-    slides[slideIndex-1].style.display = "block"
+    slides[slideIndex-1].style.display = 'block'
     dots[slideIndex-1].className += ' active'
 }
